70d4605b7e8b2bc02ea770ab203e4846809e0775
the16thpythonist <jonseb1998@gmail.com>
2020-03-27 14:14:54 +0100

    Added the basic Vue source code

---
+++ src/the16thpythonist/Wordpress/wpcommands/src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>{{ message }}</div>
+</template>
+
+<script>
+    export default {
+        name: "Test",
+        props: {
+            message: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
