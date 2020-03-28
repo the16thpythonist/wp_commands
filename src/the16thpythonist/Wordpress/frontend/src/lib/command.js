bed62165ac2c8cffd022b6efe8145408ca6c8360
the16thpythonist <jonseb1998@gmail.com>
2020-03-28 16:38:42 +0100

    Added some basic comment stubs

--- src/the16thpythonist/Wordpress/frontend/src/lib/command.js
+++ src/the16thpythonist/Wordpress/frontend/src/lib/command.js
@@ -1,4 +1,15 @@
-
+/**
+ *
+ * CHANGELOG
+ *
+ * Added 28.03.2020
+ *
+ * @param name
+ * @param defaultValue
+ * @param type
+ * @param optionalFlag
+ * @constructor
+ */
 function CommandParameter(name, defaultValue, type, optionalFlag) {
     this.name = name;
     this.default = defaultValue;
@@ -6,11 +17,32 @@ function CommandParameter(name, defaultValue, type, optionalFlag) {
     this.optional = optionalFlag;
 }
 
+/**
+ *
+ * CHANGELOG
+ *
+ * Added 28.03.2020
+ *
+ * @param name
+ * @param parameters
+ * @constructor
+ */
 function Command(name, parameters) {
     this.name = name;
     this.parameters = parameters;
 }
 
+/**
+ *
+ * CHANGELOG
+ *
+ * Added 28.03.2020
+ *
+ * @param name
+ * @param dateTime
+ * @param logPath
+ * @constructor
+ */
 function CommandExecution(name, dateTime, logPath) {
     this.name = name;
     this.time = dateTime;
