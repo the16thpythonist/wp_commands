b87c382486123dd21a3dc48c1a175ae5025980a4
the16thpythonist <jonseb1998@gmail.com>
2020-03-28 16:27:30 +0100

    fixed the issue with the vue code being compiled into a module instead of a component

---
+++ src/the16thpythonist/Wordpress/frontend/src/components/RecentCommands.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "RecentCommands"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/the16thpythonist/Wordpress/frontend/src/components/WpCommandsWidget.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "WpCommandsWidget"
+    }
+</script>
+
+<style scoped>
+
+</style>
