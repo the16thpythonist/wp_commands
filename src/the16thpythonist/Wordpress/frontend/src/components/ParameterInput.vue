1668b19e0d603992b6a4aea2cab87f9c713a3503
the16thpythonist <jonseb1998@gmail.com>
2020-04-19 16:13:32 +0200

    extended parameter input component

--- src/the16thpythonist/Wordpress/frontend/src/components/ParameterInput.vue
+++ src/the16thpythonist/Wordpress/frontend/src/components/ParameterInput.vue
@@ -45,10 +45,61 @@
                 return this.command.parameters;
             }
         },
+        watch: {
+            /**
+             * CHANGELOG
+             *
+             * Added 29.03.2020
+             */
+            command: function (newCommand, oldCommand) {
+                if (newCommand !== oldCommand) {
+                    this.fillDefaultValues();
+                }
+            }
+        },
         methods: {
+            /**
+             * Callback for the "input" event on all the input fields of the component.
+             *
+             * This method is being called every time ANY(!) one of the input fields for the parameter has its value
+             * changed by the user. This method in turn then also emits an "input" event to be seen by the parent
+             * component. Passing the parameterValues as a parameter to the event.
+             * This functionality is required to support the v-model functionality with the parameter values of this
+             * component.
+             *
+             * CHANGELOG
+             *
+             * Added 28.03.2020
+             */
             handleInput: function (e) {
                 this.$emit('input', this.parameterValues);
+            },
+            /**
+             * Sets the parameter values for all optional parameters to their supplied default values.
+             *
+             * This function will iterate through all the of the parameters of the subject command.
+             *
+             * CHANGELOG
+             *
+             * Added 29.03.2020
+             */
+            fillDefaultValues: function() {
+                this.parameters.forEach((parameter) => {
+                    if (parameter.optional) {
+                        this.parameterValues = parameter.default;
+                    }
+                });
             }
+        },
+        /**
+         *
+         *
+         * CHANGELOG
+         *
+         * Added 29.03.2020
+         */
+        created: function () {
+            this.fillDefaultValues();
         }
     }
 </script>
